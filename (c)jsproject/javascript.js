c0b156cf1d1f172d6747838be3e81df23e9f0d3a
SakrFeat <122815359+SakrFeat@users.noreply.github.com>
2023-04-20 11:37:42 +0300

    Add files via upload

---
+++ (c)jsproject/javascript.js
@@ -0,0 +1,8 @@
+
+function onClickButton() {
+  if (document.getElementById('menu').style.display=='')
+  document.getElementById('menu').style.display='block';
+  else {
+    document.getElementById('menu').style.display='';
+  }
+}
