52d18d7f8993fd67fac57653c6f69953855880b4
SakrFeat <122815359+SakrFeat@users.noreply.github.com>
2023-04-26 22:14:11 +0300

    Delete (c)jsproject directory

--- (c)jsproject/javascript.js
+++
@@ -1,8 +0,0 @@
-
-function onClickButton() {
-  if (document.getElementById('menu').style.display=='')
-  document.getElementById('menu').style.display='block';
-  else {
-    document.getElementById('menu').style.display='';
-  }
-}
